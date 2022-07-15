1c05c44ff1b0d4e16a07fc5447eaa8309f4884b3
TheHaloEffect <c17442074@mytudublin.ie>
2022-07-15 15:55:22 +0100

    changed API, got it working

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-const api_url ="https://zenquotes.io/api/random/";
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
 
-async function fetchQuote(api_url)
+// Fetch quote from API
+async function fetchQuote()
 {
-  const response = await fetch(api_url);
-  const data = await response.json();
-  console.log(data[0]);
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+
+        if (data.authorText === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        quoteText.innerText = data.quoteText;
+        
+    } catch (error) {
+        // If there is an error, run function again
+        fetchQuote();
+        console.log(error.message);
+    }  
 }
 
-fetchQuote(api_url);
+fetchQuote();
