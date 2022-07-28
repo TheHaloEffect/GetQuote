86b6d45689e8e863c4dfcc37144988e184d37fba
TheHaloEffect <c17442074@mytudublin.ie>
2022-07-28 14:09:34 +0100

    removed cookie feature, easier to implement with Node.js and Express

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
+const quoteLink = document.getElementById('quote-link');
+const quoteBtn = document.getElementById('quote-btn');
+const languageValue = document.getElementById('change-lang');
 
 // Fetch quote from API
 async function fetchQuote()
 {
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    let language = languageValue.value;
+
+    // Sending request through proxy to bypass CORS error
+    let apiUrl = `https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=${language}`
 
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        console.log(data);
 
+        // If author is blank, set to Unknown
         if (data.authorText === '') {
             authorText.innerText = 'Unknown';
         } else {
             authorText.innerText = data.quoteAuthor;
         }
+        
         quoteText.innerText = data.quoteText;
+        quoteLink.setAttribute('href', data.quoteLink)
         
     } catch (error) {
         // If there is an error, run function again
         fetchQuote();
-        console.log(error.message);
     }  
 }
 
+quoteBtn.addEventListener('click', fetchQuote);
+
+// On load (loads a quote when page loads)
 fetchQuote();
